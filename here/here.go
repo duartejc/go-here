46c616153673df6c511ce3b86899d31a598b090a
Jean Duarte <jeancduarte@gmail.com>
2020-03-24 20:10:23 -0300

    changed url

--- here/here.go
+++ here/here.go
@@ -20,7 +20,7 @@ type Client struct {
 
 // NewRoutingClient returns a new RoutingClient.
 func NewRoutingClient(httpClient *http.Client) *Client {
-	base := sling.New().Client(httpClient).Base("https://route.api.here.com/routing/7.2/")
+	base := sling.New().Client(httpClient).Base("https://route.ls.hereapi.com/routing/7.2/")
 	return &Client{
 		sling:   base,
 		Routing: newRoutingService(base.New()),
