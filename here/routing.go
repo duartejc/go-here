f91b35892d49946ce34a57db4261fc81d52791ac
Jean Duarte <jeancduarte@gmail.com>
2020-03-30 17:57:52 -0300

    removed geo

--- here/routing.go
+++ here/routing.go
@@ -143,9 +143,9 @@ func newRoutingService(sling *sling.Sling) *RoutingService {
 // Returns waypoints as a formatted string.
 func createWaypoint(waypoint WaypointParams) string {
 	if waypoint.StopOver > 0 {
-		return fmt.Sprintf("geo!stopOver,%d!%f,%f;", waypoint.StopOver, waypoint.Coordinates[0], waypoint.Coordinates[1])
+		return fmt.Sprintf("stopOver,%d!%f,%f;", waypoint.StopOver, waypoint.Coordinates[0], waypoint.Coordinates[1])
 	} else {
-		return fmt.Sprintf("geo!%f,%f;", waypoint.Coordinates[0], waypoint.Coordinates[1])
+		return fmt.Sprintf("%f,%f;", waypoint.Coordinates[0], waypoint.Coordinates[1])
 	}
 }
 
